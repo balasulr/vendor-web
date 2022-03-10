12af5cfa5463c6232e9c88f06e62cc6acd17b50e
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-03-10 16:40:53 -0500

    Initial commit

---
+++ src/main/java/com/maxtrain/bootcamp/Invoiceline/InvoicelineRepository.java
@@ -0,0 +1,7 @@
+package com.maxtrain.bootcamp.Invoiceline;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InvoicelineRepository extends CrudRepository<Invoiceline, Integer> {
+
+}
---
+++ src/main/java/com/maxtrain/bootcamp/invoice/Invoice.java
@@ -0,0 +1,61 @@
+package com.maxtrain.bootcamp.invoice;
+
+import javax.persistence.*;
+
+import com.maxtrain.bootcamp.vendor.Vendor;
+
+@Entity
+public class Invoice {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(length=30, nullable=false)
+	private String description;
+	@Column(length=30, nullable=false)
+	private String status;
+	@Column(columnDefinition="decimal(9,2) NOT NULL DEFAULT 0.0")
+	private double total;
+	
+	@ManyToOne(optional=false)
+	@JoinColumn(name="vendorId",columnDefinition="int") // From Vendor
+	private Vendor vendor; // Foreign Key to Vendor
+
+	public Invoice() {}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	public Vendor getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(Vendor vendor) {
+		this.vendor = vendor;
+	}
+	
+}
